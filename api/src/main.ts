caf399604f2947605113bc55e3399970d5aa3364
Saint48198 <daniels.scott.r@gmail.com>
2025-11-30 14:34:59 -0500

    added endpoints for password and user token

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,13 +1,24 @@
 import express from 'express';
+import cookieParser from 'cookie-parser';
 
+import userTokenRouter from './routes/userToken';
 import usersRouter from './routes/users';
 import userPasswordRoute from './routes/userPassword';
+import changePasswordRouter from './routes/changePassword';
+
 
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use(usersRouter);
 app.use(userPasswordRoute);
+app.use(changePasswordRouter);
+app.use(userTokenRouter);
 
-export default app;
+const port = process.env.PORT || 3001;
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
